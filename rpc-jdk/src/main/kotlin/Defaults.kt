8f0fbb5ff632a26a9c17c3db585fa3bd2311ef84
Jay Jeyaruban <jayjeyaruban@gmail.com>
2025-03-26 23:41:05 +0000

    feat(rpc): concurrent map based di context impl

--- rpc-jdk/src/main/kotlin/Defaults.kt
+++ rpc-jdk/src/main/kotlin/Defaults.kt
@@ -1,7 +1,26 @@
 package jstack.rpc.jdk
 
 import jstack.core.Loader
+import jstack.core.Type
+import jstack.di.DiContext
+import java.util.concurrent.ConcurrentHashMap
 import java.util.concurrent.Executor
 import java.util.concurrent.Executors
 
-val Executor = jstack.core.Loader<Any, Executor> { Executors.newVirtualThreadPerTaskExecutor() }
+val Executor = Loader<Any, Executor> { Executors.newVirtualThreadPerTaskExecutor() }
+
+class ConcurrentMapDiContext() : DiContext {
+    private val storage = ConcurrentHashMap<Type<*>, Any?>()
+    private val path = mutableSetOf<Type<*>>()
+
+    override fun <T> getOrDefault(t: Type<T>, f: () -> T): T = t.cast(
+        storage.computeIfAbsent(t) {
+            if (t in path) {
+                error("Cyclic dependency detected: $path")
+            }
+            path.add(t)
+
+            f().also { path.remove(t) }
+        },
+    )
+}
