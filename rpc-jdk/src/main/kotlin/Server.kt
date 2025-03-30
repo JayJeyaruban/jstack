513797abbef3d2beff63cbdaa71d33d9b82d0e7c
Jay Jeyaruban <jayjeyaruban@gmail.com>
2025-03-30 19:25:39 +0100

    refactor(rpc): move request handling behind interface

--- rpc-jdk/src/main/kotlin/Server.kt
+++ rpc-jdk/src/main/kotlin/Server.kt
@@ -1,11 +1,9 @@
 package jstack.rpc.jdk
 
 import com.sun.net.httpserver.HttpContext
-import com.sun.net.httpserver.HttpExchange
 import com.sun.net.httpserver.HttpServer
 import jstack.di.DiContext
 import jstack.di.retrieve
-import jstack.rpc.ProcedureRoute
 import jstack.rpc.Router
 import jstack.rpc.traverse
 
@@ -14,28 +12,8 @@ typealias HttpContextConfigurer = HttpContext.() -> Unit
 fun <C : DiContext> C.install(server: HttpServer, router: Router<C>, f: HttpContextConfigurer? = null) = server.apply {
     executor = retrieve(Executor)
 
-    val codec = retrieve(Codec)
+    val adapter = retrieve(ProcedureAdapter)
     router.traverse { path, proc ->
-        createContext("/$path/") { ex ->
-            if (ex.requestMethod == "POST") {
-                execute(proc, codec, ex)
-            } else {
-                ex.sendResponseHeaders(405, -1)
-            }
-
-            ex.close()
-        }.apply { f?.invoke(this) }
-    }
-}
-
-private fun <C : DiContext, I, O> C.execute(proc: ProcedureRoute<C, I, O>, codec: Codec, ex: HttpExchange) {
-    try {
-        val args = codec.read(ex.requestBody, proc.input)
-        val response = proc(this)(args)
-        ex.sendResponseHeaders(200, 0)
-        codec.write(ex.responseBody, response)
-    } catch (e: Throwable) {
-        e.printStackTrace()
-        ex.sendResponseHeaders(500, -1)
+        createContext("/$path/", adapter.run { adapt(proc) }).apply { f?.invoke(this) }
     }
 }
